6b9c21a05401d9ca9d3972ea0942bde73cb6dbb4
Onat <53895969+onattech@users.noreply.github.com>
2023-03-05 17:50:31 +0300

    Add custom endpoint component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,5 +1,6 @@
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents"
+import Endpoint from "../components/Endpoint"
 import SampleEmail from "../components/SampleEmail"
 
 export default {
@@ -8,4 +9,5 @@ export default {
     // Map the "sampleEmail" tag to our <SampleEmail /> component!
     // `SampleEmail` will receive all props that were passed to `sampleEmail` in MDX
     sampleEmail: SampleEmail,
+    endpoint: Endpoint,
 }
